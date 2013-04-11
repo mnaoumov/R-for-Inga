c967823ffff6047c2288f1aab3bd7b34499a4a6c
Michael Naumov <michael.naumov@readify.net>
2013-04-11 21:58:51 +1000

    Improve comment for set.seed

--- SP Block commands.R
+++ SP Block commands.R
@@ -5,6 +5,7 @@ source('SP Block functions.R')
 
 # make predictable 'random' results
 # 12345 could be changed to different value to produce other 'random' results
+# comment out next line if you need truly random value
 set.seed(12345)
 
 b = 7
