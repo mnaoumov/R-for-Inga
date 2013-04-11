4443633a0c057e0a6a7cc0328e882f7ccff8e90a
Michael Naumov <michael.naumov@readify.net>
2013-04-11 21:41:13 +1000

    Add predictable random results

--- SP Block commands.R
+++ SP Block commands.R
@@ -2,3 +2,7 @@
 setwd("c:/Inga/")
 
 source('SP Block functions.R')
+
+# make predictable 'random' results
+# 12345 could be changed to different value to produce other 'random' results
+set.seed(12345)
